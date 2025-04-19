26c6f6e8b8983022716d7bd9049c50538b6bf0d5
Sazib018 <mahiburislamsazib@gmail.com>
2025-04-19 11:27:07 -0500

    dded

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+// Create an Express application
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// Middleware setup
+app.use(cors()); 
+app.use(express.json()); 
 
-// MongoDB URI
+// MongoDB connection URI using credentials from environment variables
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjpks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a new MongoDB client
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,18 +23,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+// Async function to connect to MongoDB and define routes
 async function run() {
     try {
-       
+        // Connect to the database and collections
         const db = client.db("job_portal_server");
         const JobsCollection = db.collection("jobs");
         const ApplicationsCollection = db.collection("applications");
 
+        // ------------------- JOBS API -------------------
+
+        // Get all jobs
         app.get("/jobs", async (req, res) => {
             const result = await JobsCollection.find().toArray();
             res.send(result);
         });
 
+        // Get a specific job by ID
         app.get("/jobs/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -40,12 +47,35 @@ async function run() {
             res.send(result);
         });
 
+
+        // Add a new job
         app.post("/jobs", async (req, res) => {
             const data = req.body;
             const result = await JobsCollection.insertOne(data);
             res.send(result);
         });
 
+        // Update a job by ID
+        app.put("/jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedJob = req.body;
+            const result = await JobsCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedJob }
+            );
+            res.send(result);
+        });
+
+        // Delete a job by ID
+        app.delete("/jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await JobsCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+        // ------------------- APPLICATIONS API -------------------
+
+        // Submit a job application
         app.post("/applications", async (req, res) => {
             const application = req.body;
             const result = await ApplicationsCollection.insertOne(application);
@@ -58,7 +88,7 @@ async function run() {
             res.send(result);
         });
 
-        // Delete  application 
+        // Delete an application by ID
         app.delete("/applications/:id", async (req, res) => {
             const id = req.params.id;
             const result = await ApplicationsCollection.deleteOne({ _id: new ObjectId(id) });
@@ -70,8 +100,10 @@ async function run() {
     }
 }
 
+// Call the run function and handle any errors
 run().catch(console.dir);
 
+// Start the Express server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
